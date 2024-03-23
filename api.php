7ae78f6c07c973ae0a572a83735b32aa99130201
David Salgado <115661362+YoDavidSalgado@users.noreply.github.com>
2024-03-22 19:51:20 -0500

    Update api.php

--- api.php
+++ api.php
@@ -2,7 +2,7 @@
     include('conectDB.php');
     $identifi=$_GET["id"];
     if($resultados=obtenerResultado("SELECT * FROM `usuarios` WHERE id='$identifi'")){
-        while($fila=$resultados->fecth_array(MYSQLI_NUM)){
+        while($fila=$resultados->fetch_array(MYSQLI_NUM)){
             echo json_encode($fila);
         }
     }
